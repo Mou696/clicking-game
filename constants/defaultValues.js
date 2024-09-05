dcf2876fd0859c60c2aa06a9953b13912b0b5aa0
Mohammad Safdari <safdarimohammed10@gmail.com>
2024-09-05 09:43:18 +0300

    images replaced

--- constants/defaultValues.js
+++ constants/defaultValues.js
@@ -1,17 +1,17 @@
 export const defaultUpgradeValues = [
-    {name: 'clicker', image: './assets/clicker-white.png', cost: 10, increase: 1, type: "upgrade"},
-    {name: 'pickaxe', image: './assets/pickaxe.png', cost: 100, increase: 3, type: "upgrade"},
-    {name: 'miner', image: './assets/miner.png', cost: 551, increase: 25, type: "upgrade"},
-    {name: 'factory', image: './assets/factory.png', cost: 50000, increase: 230, type: "upgrade"},
-    {name: 'potion', image: './assets/potion.png', cost: 600000, increase: 4500, type: "upgrade"},
-    {name: 'Nuke', image: './assets/Nuke.png', cost: 1000000, increase: 7000, type: "upgrade"}
+    {name: 'clicker', image: 'clicker-white.png', cost: 10, increase: 1, type: "upgrade"},
+    {name: 'pickaxe', image: 'pickaxe.png', cost: 100, increase: 3, type: "upgrade"},
+    {name: 'miner', image: 'miner.png', cost: 551, increase: 25, type: "upgrade"},
+    {name: 'factory', image: 'factory.png', cost: 50000, increase: 230, type: "upgrade"},
+    {name: 'potion', image: 'potion.png', cost: 600000, increase: 4500, type: "upgrade"},
+    {name: 'Nuke', image: 'Nuke.png', cost: 1000000, increase: 7000, type: "upgrade"}
   ]
   
   export const defaultSkillValues = [
     {
      name: "Stronger Clicks",
      description: "Double your clicking power for 30 seconds",
-     image: "./assets/skills/skill1.png",
+     image: "skill1.png",
      cd: 600,
      cost: 12000,
      type: "skill"
@@ -19,7 +19,7 @@ export const defaultUpgradeValues = [
     {
      name: "Lucky Day",
      description: "Gain 600 x GPS worth of gems instantly",
-     image: "./assets/skills/skill1.png",
+     image: "skill1.png",
      cd: 900,
      cost: 480000,
      type: "skill"
